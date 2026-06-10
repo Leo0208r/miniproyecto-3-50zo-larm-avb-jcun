36ccf81eac62bfbd5945eacf573724e34abc7260
Leo0208r <roseroalexis04@gmail.com>
2026-06-10 11:33:38 -0500

    add card and player class

--- src/main/java/com/example/_0zo/model/players/Player.java
+++ src/main/java/com/example/_0zo/model/players/Player.java
@@ -1,7 +1,21 @@
-package com.example._0zo.model;
+package com.example._0zo.model.players;
+
+import com.example._0zo.model.Card;
+import com.example._0zo.model.enums.PlayerStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Player {
-    protected final String name="";
+    protected final String name;
+    protected final List<Card> hand;
+    protected PlayerStatus status;
+    public Player(String name){
+        this.name=name;
+        this.hand= new ArrayList<>();
+        this.status=PlayerStatus.ACTIVE;
+    }
+
 
 
 }
